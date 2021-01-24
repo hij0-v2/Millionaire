f9e13bbb72b479d4678a9cdaf3aa7157305b3ec6
Ramaleu <ramunas8250@gmail.com>
2021-01-24 17:37:28 +0200

    SOUND

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -2,33 +2,46 @@ import React, { useState } from 'react';
 import data from '../data/Data'
 import './main.css'
 import Game from '../game/Game'
+import dyingAnim from '../resources/images/dyingAnim.gif'
+import dyingSound from '../resources/sound/dyingSound.mp3'
+
 
 
 
 function Main(){
   let [start, setStart] = useState(true);
   let [gameState, setOver] =useState(false);
+  
 
   const startGame = () => {
-    console.log(Game.target)
     setStart(false);
   }
   const gameOver = () => {
-    console.log(gameState);
     setOver(true);
-    console.log(gameState);
   }
+
+  const setHalfVolume = () => {
+    setTimeout(() => {      let myAudio = document.getElementById("audio");  
+    myAudio.volume=0.2 ; }, 1);
+  }
+
 
 
   return(
     <div id="area">
       {start === true &&
       <div id="startBox">
-        <p>WHAT IS GOING ON</p>
+        <p>WHO WANTS TO BE A WEB DEVELOPER</p>
         <button onClick={() => startGame()} classname="startButton">Start</button>
       </div>}
       {start === false && gameState === false && <Game target={gameOver}/>}
-      {gameState ===true && <p><h1>GAME OVER</h1></p>}
+      {gameState ===true && <div>
+      <img src = {dyingAnim}/>
+      <audio id="audio" autoPlay="autoplay" onLoadedData={setHalfVolume()}>
+      <source src= {dyingSound} type="audio/mpeg"/>
+      </audio>
+        <p><h1>GAME OVER</h1></p>
+        </div>}
     </div>
   )
 }
